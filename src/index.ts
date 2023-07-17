5fcc4866cd5d913f955edeb577e53cc0d804cfaa
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2023-07-17 20:55:54 +0700

    Tambahkan List dan Update
    Added
    - List perusahaan
    - List Barang
    - Update Perusahaan

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,11 @@ app.get('/perusahaan/:id', async(req, res) =>{
     res.json(detail_perusahaan)
 })
 
+app.get('/perusahaan', async(req, res) =>{
+    const list_Perusahaan = await prisma.perusahaan.findMany()
+    res.json(list_Perusahaan)
+})
+
 app.post('/perusahaan', async(req, res) =>{
     const { nama, kode, alamat, no_telp} = req.body
     const result = await prisma.perusahaan.create({
@@ -27,12 +32,40 @@ app.post('/perusahaan', async(req, res) =>{
     res.json(result)
 })
 
+//FIX ME : ini codenya untested, tolong debug dulu 
+app.put('/perusahan/:id', async(req, res)=>{
+    const { nama, kode, alamat, no_telp} = req.body
+
+    try {
+        const perusahaanData = await prisma.perusahaan.findUnique({
+            where: {id : req.params.id}
+        })
+
+        const updatedPerusahaan = await prisma.perusahaan.update({
+            where: { id : req.params.id || undefined },
+            data : {
+                nama,
+                kode, 
+                alamat, 
+                no_telp
+            }
+        })
+    } catch (error) {
+        res.json({error : 'Tidak ada perusahaan dengan ID ${id}'})
+    }
+})
+
 app.get('/barang/:id', async(req, res) =>{
 
     const detail_barang = await prisma.barang.findUnique({
         where: { kode : req.params.id}
     })
     res.json(detail_barang)
+})
+
+app.get('/barang', async(req, res)=>{
+    const list_barang = await prisma.barang.findMany()
+    res.json(list_barang); 
 })
 
 app.post('/barang', async(req, res) =>{
