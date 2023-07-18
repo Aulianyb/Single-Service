13237afd08579bf9badeb0eccbbd303ec96d19f1
Aulia Nadhirah Yasmin Badrulkamal <Aulianybk123@gmail.com>
2023-07-19 02:40:13 +0700

    Added Update and Delete

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 import express from 'express'
+import { User, Barang, Perusahaan, Response, BarangWithPerusahaan } from './models/models';
 const port = process.env.PORT || 5000; 
+import jwt from 'jsonwebtoken'; 
 
 const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', 'https://ohl-fe.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next(); 
+})
 
 app.get('/perusahaan/:id', async(req, res) =>{
     const detail_perusahaan = await prisma.perusahaan.findUnique({
@@ -29,11 +38,10 @@ app.post('/perusahaan', async(req, res) =>{
             no_telp
         }, 
     })
-    res.json(result)
+    res.json(`Berhasil menambahkan ${result.nama}!`)
 })
 
-//FIX ME : ini codenya untested, tolong debug dulu 
-app.put('/perusahan/:id', async(req, res)=>{
+app.put('/perusahaan/:id', async(req, res)=>{
     const { nama, kode, alamat, no_telp} = req.body
 
     try {
@@ -46,12 +54,24 @@ app.put('/perusahan/:id', async(req, res)=>{
             data : {
                 nama,
                 kode, 
-                alamat, 
+                alamat,
                 no_telp
             }
         })
+        res.json(`Berhasil update ${updatedPerusahaan.nama}!`); 
     } catch (error) {
-        res.json({error : 'Tidak ada perusahaan dengan ID ${id}'})
+        res.json({error : `Tidak ada perusahaan dengan ID ${req.params.id}`})
+    }
+})
+
+app.delete('/perusahaan/:id', async(req, res)=>{
+    try {
+        const perusahaanData = await prisma.perusahaan.delete({
+            where: {id : req.params.id}
+        })
+        res.json(`Berhasil menghapus ${perusahaanData.nama}!`); 
+    } catch {
+        res.json({error : `Tidak ada perusahaan dengan ID ${req.params.id}`})
     }
 })
 
@@ -79,9 +99,60 @@ app.post('/barang', async(req, res) =>{
             perusahaan_id
         }
     })
-    res.json(result)
+    res.json(`Berhasil menambahkan ${result.nama}!`)
+})
+
+app.put('/barang/:id', async (req,res)=>{
+    const {kode, nama, harga, stok, perusahaan_id} = req.body
+    try {
+        const barangData = await prisma.barang.findUnique({
+            where: {id : req.params.id}
+        })
+
+        const updatedBarang = await prisma.barang.update({
+            where: { id : req.params.id || undefined },
+            data : {
+                kode,
+                nama, 
+                harga,
+                stok, 
+                perusahaan_id
+            }
+        })
+        res.json(`Berhasil update ${updatedBarang.nama}!`);  
+    } catch (error) {
+        res.json({error : `Tidak ada barang dengan ID ${req.params.id}`})
+    }
+})
+
+app.delete('/barang/:id', async (req, res) =>  {
+   try {
+        const barangData = await prisma.barang.delete({
+            where: {id : req.params.id}
+        })
+        res.json(`Berhasil menghapus ${barangData.nama}!`);  
+   } catch {
+        res.json({error : `Tidak ada barang dengan ID ${req.params.id}`})
+   } 
+})
+
+app.post('/login', async (req, res) =>{
+    const {username, password} = req.body
+    try{
+        const adminData = await prisma.user.findFirst({
+            where : {username : username}
+        })
+
+        if (adminData?.password == password){
+
+        }
+    } catch {
+
+    }
+    console.log("Testing lmao"); 
 })
 
 app.listen(port, ()=> {
+    
     console.log(`Server is running on port: ${port}`); 
 });
